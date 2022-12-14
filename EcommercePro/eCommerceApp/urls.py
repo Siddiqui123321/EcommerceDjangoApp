ba52d023bb404420791b298933233568dd7ac503
Hamza Siddiqui <mohdhumzasid@gmail.com>
2022-12-14 22:17:27 +0530

    reinitialized files

---
+++ EcommercePro/eCommerceApp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from eCommerceApp import views
+
+urlpatterns = [
+    path('',views.index,name="index"),
+    path('contact',views.contact,name="contact"),
+    path('about',views.about,name="about"),
+] 
---
+++ EcommercePro/eCommerceApp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from eCommerceApp.models import Contact, Product
+from django.contrib import messages
+from math import ceil
+
+# Create your views here.
+def index(request):
+
+    allProds = []
+    catprods = Product.objects.values('category','id')
+    print(catprods)
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod= Product.objects.filter(category=cat)
+        n=len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    params= {'allProds':allProds}
+
+    return render(request,"index.html",params)
+
+
+
+def contact(request):
+    if request.method=="POST":
+        name=request.POST.get("name")
+        email=request.POST.get("email")
+        desc=request.POST.get("desc")
+        pnumber=request.POST.get("pnumber")
+        myquery=Contact(name=name,email=email,desc=desc,phonenumber=pnumber)
+        myquery.save()
+        messages.info(request,"we will get back to you soon..")
+        return render(request,"contact.html")
+    
+    return render(request,"contact.html")
+    
+def about(request):
+    return render(request,"about.html")
+
